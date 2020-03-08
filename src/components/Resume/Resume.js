0f3bef9d10a24aacba55d3c93058380f3d28b65a
Daniel Cortes <danncortes@gmail.com>
2020-03-08 10:11:10 +0100

    Starting...

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Resume = () => {
+  return (
+    <section className="resume p-6 bg-gray-200 h-screen">
+      <div className="resume__cont max-w-screen-lg mx-auto">
+        Resume
+      </div>
+    </section>
+  )
+}
