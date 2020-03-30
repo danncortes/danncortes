79d8219357f078ca631404ba4a9ac748e3ccfff4
Daniel Cortes <danncortes@gmail.com>
2020-03-30 14:48:44 +0200

    Updates

---
+++ precache-manifest.6e6d359138b447d1a3b89454a5557cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae1831322787013d9a7e3a5aa6c0e3c",
+    "url": "/danncortes/index.html"
+  },
+  {
+    "revision": "8f12a73f74ac10b85cc1",
+    "url": "/danncortes/static/css/main.1695d73c.chunk.css"
+  },
+  {
+    "revision": "2e6586cfb9dd932e8ed8",
+    "url": "/danncortes/static/js/2.47678e9c.chunk.js"
+  },
+  {
+    "revision": "98d15dd566f2d3be3c99e71112d22e2b",
+    "url": "/danncortes/static/js/2.47678e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f12a73f74ac10b85cc1",
+    "url": "/danncortes/static/js/main.4c7fe4f3.chunk.js"
+  },
+  {
+    "revision": "6a38a124ffe6cfd843b0",
+    "url": "/danncortes/static/js/runtime-main.3813c62e.js"
+  },
+  {
+    "revision": "b6df10dad68117642509d0ea2732cf1c",
+    "url": "/danncortes/static/media/profile-pic-md.b6df10da.png"
+  }
+]);
