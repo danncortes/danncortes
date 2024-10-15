4d93d88fc34528fe3af7c30064c46ea8ae5c163f
Daniel Cortes <danncortes@gmail.com>
2024-10-15 16:32:12 +0200

    localization Part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Resume from './components/Resume';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/">
-            <Resume />
-          </Route>
+          <Route path="/:lang" component={Resume}></Route>
+          <Route path="/*" component={Resume}></Route>
         </Switch>
       </div>
     </Router>
--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -5,38 +5,38 @@ export type Technology = {
   name: string;
   experience: number;
   experienceLevel: React.ReactNode;
-  showExperience: boolean;
+  showExpOnPrint: boolean;
 };
 
 export const Technology = ({
   name,
   experience,
   experienceLevel,
-  showExperience
+  showExpOnPrint
 }: Technology) => {
   const { t } = useTranslation();
-  const showExp: boolean = Boolean(experience) && showExperience;
 
   const yearsExperience: string = ((): string => {
-    if (showExp) {
-      const yearsExperience: number = Math.round((experience / 12) * 10) / 10;
-      if (yearsExperience > 1 && yearsExperience % 1 > 0) {
-        return `${Math.floor(yearsExperience)}+`;
-      }
-      return `${yearsExperience}`;
+    const yearsExperience: number = Math.round((experience / 12) * 10) / 10;
+    if (yearsExperience > 1 && yearsExperience % 1 > 0) {
+      return `${Math.floor(yearsExperience)}+`;
     }
-    return '';
+    return `${yearsExperience}`;
   })();
 
   return (
     <div className="technology">
       <h4 className="technology__title">{name}</h4>
       {experienceLevel}
-      {showExp && (
-        <p className="technology__experience">
+      {
+        <p
+          className={`technology__experience ${
+            !showExpOnPrint ? 'no-show-print' : ''
+          }`}
+        >
           {`${yearsExperience} ${t('yOfExp')}`}
         </p>
-      )}
+      }
     </div>
   );
 };
