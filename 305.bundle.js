5c4c7669089185a705716b094a7179a0745609d9
Daniel Cortes <danncortes@gmail.com>
2024-10-30 15:23:55 +0100

    Updates

--- 305.bundle.js
+++ 305.bundle.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkdanncortes=self.webpackChunkdanncortes||[]).push([[305],{3305:(e,s,a)=>{a.r(s),a.d(s,{default:()=>c});const c=a.p+"images/truelogic-130.avif"}}]);
+"use strict";(self.webpackChunkdanncortes=self.webpackChunkdanncortes||[]).push([[305],{305:(e,s,a)=>{a.r(s),a.d(s,{default:()=>c});const c=a.p+"images/truelogic-130.avif"}}]);
--- 946.bundle.js
+++ 946.bundle.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkdanncortes=self.webpackChunkdanncortes||[]).push([[946],{6946:(e,s,a)=>{a.r(s),a.d(s,{default:()=>n});const n=a.p+"images/velocity-partners-260.avif"}}]);
+"use strict";(self.webpackChunkdanncortes=self.webpackChunkdanncortes||[]).push([[946],{946:(e,s,a)=>{a.r(s),a.d(s,{default:()=>n});const n=a.p+"images/velocity-partners-260.avif"}}]);
